5ceffbfa7af4b847b8c59af440216aafaf6bf8dd
zakariaMagdy <59423819+zakariaMagdy@users.noreply.github.com>
2020-07-10 23:44:30 +0200

    asds

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const errorControlelr = require("./Controller/errorController");
 const userRouter = require("./Routes/userRoutes");
 const profileRouter = require("./Routes/profileRoutes");
 const postRouter = require("./Routes/postRoutes");
+const path = require("path");
 
 const app = express();
 
@@ -19,6 +20,15 @@ app.use(express.static("Client/build"));
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/profiles", profileRouter);
 app.use("/api/v1/posts", postRouter);
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.all("*", function (req, res, next) {
   return next(new HttpError("This route is not valid!", 404));
