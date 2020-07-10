e784f0618fa61e0ff2281e49f1fb15a1ec567ed6
zakariaMagdy <59423819+zakariaMagdy@users.noreply.github.com>
2020-07-11 01:31:54 +0200

    hi

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/posts", postRouter);
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
