db801460ff783987517c849063ea35c1a37718ea
zakariaMagdy <59423819+zakariaMagdy@users.noreply.github.com>
2020-07-11 02:07:30 +0200

    sd

--- Models/User.js
+++ Models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const jwt = require("jsonWebToken");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 
